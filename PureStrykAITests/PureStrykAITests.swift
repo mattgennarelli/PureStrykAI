bde0bedcb5b425274fde54c0dc4a44c822766325
mattgennarelli <matthewgennarelli@@gmail.com>
2025-03-12 16:21:35 -0400

    Initial Commit

---
+++ PureStrykAITests/PureStrykAITests.swift
@@ -0,0 +1,17 @@
+//
+//  PureStrykAITests.swift
+//  PureStrykAITests
+//
+//  Created by Matthew Gennarelli on 3/12/25.
+//
+
+import Testing
+@testable import PureStrykAI
+
+struct PureStrykAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
